b4344d57767b653c357d99cde92432e17ea0635c
Naivid <danielnavid03@gmail.com>
2024-02-21 12:01:04 -0500

    no chages

--- ProyectoCorte1/src/co/edu/unbosque/controller/AplMain.java
+++ ProyectoCorte1/src/co/edu/unbosque/controller/AplMain.java
@@ -1,5 +1,12 @@
 package co.edu.unbosque.controller;
 
+import co.edu.unbosque.view.*;
+
 public class AplMain {
+
+	public static void main(String[] args) {
+		Inicio in = new Inicio();
+		in.setVisible(true);
+	}
 
 }
